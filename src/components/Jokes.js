579497dfc8ae6e8c1de1f7b324722545aef9a2be
Szymon Gos <szymonngos@gmail.com>
2021-08-05 21:40:42 +0100

    create List component

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,54 +1,63 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const url = 'https://api.chucknorris.io/jokes/random'
 
 const Jokes = () => {
-    const [randomJoke, setRandomJoke] = useState();
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-  
-    const fetchRandomJoke = async () => {
-      setIsLoading(true);
-      setError(false);
-        const resp = await fetch(url);
-        const data = await resp.json()
-        setRandomJoke(data);
-        setIsLoading(false);
-    }
-  
-    useEffect(() => {
-      fetchRandomJoke();
-    }, [])
+  const [randomJoke, setRandomJoke] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    if(error){
-        return <header className='header'>
-            <h4>Network Error</h4>
-        </header>
+  const fetchRandomJoke = async () => {
+    setIsLoading(true);
+    setError(false);
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json()
+      setRandomJoke(data);
+      setIsLoading(false);
     }
+    catch (error) {
+      setError(true);
+    }
+  }
 
-    return (
-          <>
-            <header className='header'>
-              {(isLoading)
-                ? <h4>Loading...</h4>
-                : <div>
-                  <img
-                    src={randomJoke.icon_url}
-                    alt="chuck head" />
-                  <p>"{randomJoke.value}"</p>
-                </div>}
-            </header>
-            <div className="btn-container">
-              <button
-                type='button'
-                className='btn'
-                onClick={fetchRandomJoke}
-                >
-                Refresh
-              </button>
-            </div>
-          </>
-      );
+  useEffect(() => {
+    fetchRandomJoke();
+  }, [])
+
+  if (error) {
+    return <header className='header'>
+      <h4>Network Error</h4>
+    </header>
+  }
+
+  if (isLoading) {
+    return <header className='header'>
+      <h4>Loading...</h4>
+    </header>
+  }
+
+  return (
+    <>
+      <header className='header'>
+        <div>
+          <img
+            src={randomJoke.icon_url}
+            alt="chuck head" />
+          <p>"{randomJoke.value}"</p>
+        </div>
+      </header>
+      <div className="btn-container">
+        <button
+          type='button'
+          className='btn'
+          onClick={fetchRandomJoke}
+        >
+          Refresh
+        </button>
+      </div>
+    </>
+  );
 }
- 
+
 export default Jokes;
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,69 @@
+import { useRef, useReducer } from "react";
+import { nanoid } from "nanoid";
+import { AiFillCloseCircle } from 'react-icons/ai';
+
 const List = () => {
-    return ( 
+
+    const [notes, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case 'ADD': {
+                return [...state,
+                {
+                    id: nanoid(),
+                    name: action.name
+                }
+                ]
+            }
+            case 'REMOVE': {
+                return state.filter((note) => note.id !== action.id)
+            }
+            default:
+                return state;
+        }
+    }, [])
+
+    const inputEl = useRef(null);
+
+    const addNote = () => {
+        if (inputEl.current.value === '') return;
+        dispatch({
+            type: "ADD",
+            name: inputEl.current.value
+        })
+        inputEl.current.value = '';
+    }
+    console.log(notes);
+    return (
         <>
-        <header className="header">
-        <h2>List Component</h2>
-        </header>
+            <header className="header">
+                <input
+                    className='input'
+                    ref={inputEl}
+                    placeholder='Add note' />
+                <button
+                    type='button'
+                    className='btn'
+                    onClick={addNote}
+                >
+                    Add note</button>
+                <div className='notes'>
+                    {notes.map((note) => (
+                        <div
+                            key={note.id}
+                            className='notes__item'
+                        >
+                            <p>{note.name}</p>
+                            <AiFillCloseCircle 
+                            className='btn-close'
+                            onClick={()=> dispatch({ type: 'REMOVE', id: note.id})}
+                            />
+                        </div>
+
+                    ))}
+                </div>
+            </header>
         </>
-     );
+    );
 }
- 
+
 export default List;
